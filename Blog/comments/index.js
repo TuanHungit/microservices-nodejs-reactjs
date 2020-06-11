ef8d4c7b93cc6ef88c038382fffe41f532113d39
NguyenTuanHung <hung1444199@gmailc.com>
2020-06-11 22:18:06 +0700

    Create Comments Service

---
+++ Blog/comments/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const {randomBytes} = require('crypto');
+
+const app = express();
+app.use(express.json());
+
+
+const commentsByPostId = {};
+app.get('/posts/:id/comments',(req,res)=>{
+    res.status(201).send(commentsByPostId[req.params.id] || []);
+})
+
+app.post('/posts/:id/comments',(req,res)=>{
+    const commentId = randomBytes(4).toString('hex');
+    const {content} = req.body;
+    const comments = commentsByPostId[req.params.id] || [];
+    comments.push({id:commentId,content});
+    commentsByPostId[req.params.id] = comments;
+
+    res.status(201).send(comments);
+})
+
+const Port = 4001 || process.env.Port;
+app.listen(Port,()=>{
+    console.log(`Comments server is running on port ${Port}`);
+})
