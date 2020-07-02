df5959ff1ccf2789d0d0942404343054e9f5fa68
NguyenTuanHung14 <56195033+NguyenTuanHung14@users.noreply.github.com>
2020-07-02 14:12:25 +0700

    Completed App

--- Blog/client/src/App.js
+++ Blog/client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PostList from './PostList';
 import PostCreate from './PostCreate';
+
 export default()=>{
     return <div className="container">
         <h1>Create Post</h1>
--- Blog/client/src/PostList.js
+++ Blog/client/src/PostList.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
+import CommentCreate from './commentCreate'
+import CommentList from './commentList'
 export default ()=>{
     const [posts, setPosts] = useState({});
     const fetchPosts = async()=>{
@@ -18,6 +20,8 @@ export default ()=>{
             key={post.id}>
                 <div className="card-body">
                     <h3>{post.title}</h3>
+                    <CommentList postId={post.id}/>
+                    <CommentCreate postId={post.id}/>
                 </div>
             </div>
         )
