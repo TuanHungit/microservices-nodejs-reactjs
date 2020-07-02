1b6b1856698bb04af91a24cde184d483a2ce7e92
Nguyen Tuan Hung <17110156@student.hcmute.edu.vn>
2020-07-02 15:47:32 +0700

    Create event bus

--- Blog/comments/index.js
+++ Blog/comments/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {randomBytes} = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -16,10 +17,21 @@ app.post('/posts/:id/comments',(req,res)=>{
     const comments = commentsByPostId[req.params.id] || [];
     comments.push({id:commentId,content});
     commentsByPostId[req.params.id] = comments;
-
+    axios.post("http://localhost:4005/events",{
+        type:"CommentCreated",
+        data:{
+            id:commentId,
+            content,
+            postId: req.params.id
+        }
+    })
     res.status(201).send(comments);
 })
+app.post('/events',(req,res)=>{
+    console.log(`Event Comming: ${req.body.type}`)
 
+    res.send('');
+})
 const Port = 4001 || process.env.Port;
 app.listen(Port,()=>{
     console.log(`Comments server is running on port ${Port}`);
--- Blog/posts/index.js
+++ Blog/posts/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {randomBytes} = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 const app = express();
 app.use(express.json());
 const posts = {}
@@ -16,7 +17,18 @@ app.post('/posts',(req,res)=>{
     posts[id] = {
         id,title
     }
+    axios.post('http://localhost:4005/events', {
+        type:'PostCreated',
+        data:{
+            id,title
+        }
+    })
     res.status(201).send(posts[id]);
+})
+app.post('/events',(req,res)=>{
+    console.log(`Event Comming: ${req.body.type}`)
+
+    res.send('');
 })
 const Port = 4000 || process.env.PORT;
 app.listen(Port,()=>{
