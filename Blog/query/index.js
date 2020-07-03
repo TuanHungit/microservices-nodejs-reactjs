6295574b96ce46c1c798b5d141f3408e14d54e96
Nguyen Tuan Hung <17110156@student.hcmute.edu.vn>
2020-07-03 23:10:22 +0700

    Dealing with missing event

--- Blog/query/index.js
+++ Blog/query/index.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-
+const axios = require('axios')
 const app = express();
 app.use(cors());
 app.use(express.json());
 const posts  = {};
-app.get('/posts',(req,res)=>{
-    res.send(posts);
-});
-
-app.post('/events',(req,res)=>{
-    const {data,type} = req.body;
+const handleEvent = (type,data)=>{
     if(type==='PostCreated'){
         const {id,title} = data;
         posts[id] = {id, title,comments:[]};
@@ -20,9 +15,30 @@ app.post('/events',(req,res)=>{
         const post = posts[postId];
         post.comments.push({id, content, status});
     }
-    console.log(posts);
+
+    if(type==='CommentUpdated'){
+        const {id, postId,content,status} = data;
+        const post  = posts[postId];
+        const comment = post.comments.find(comment=>comment.id===id);
+        comment.status = status;
+        comment.content = content;
+    }
+}
+app.get('/posts',(req,res)=>{
+    res.send(posts);
+});
+
+app.post('/events',(req,res)=>{
+    const {data,type} = req.body;
+    handleEvent(type,data);
     res.send('');
 })
-app.listen(4002,()=>{
+app.listen(4002,async ()=>{
     console.log('Query sever is running on prt 4002');
+    const res = await axios.get('http://localhost:4005/events');
+     for(let event of res.data){
+         console.log(`Proccessing event: ${event.type}`);
+         handleEvent(event.type, event.data);
+     }
+
 })
