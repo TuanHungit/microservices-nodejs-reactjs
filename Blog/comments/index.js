086339103dabe563753b177cc0506752575295f9
Nguyen Tuan Hung <17110156@student.hcmute.edu.vn>
2020-07-03 22:19:40 +0700

    Update comment

--- Blog/comments/index.js
+++ Blog/comments/index.js
@@ -28,8 +28,26 @@ app.post('/posts/:id/comments',async(req,res)=>{
     })
     res.status(201).send(comments);
 })
-app.post('/events',(req,res)=>{
+app.post('/events',async (req,res)=>{
     console.log(`Event Comming: ${req.body.type}`)
+    const {type, data} = req.body;
+     if(type ==='CommentModerated'){
+         const {id, postId, content,status} = data;
+         const comments = commentsByPostId[postId];
+
+         const comment = comments.find(comment => comment.id === id);
+         comment.status = status;
+
+         await axios.post('http://localhost:4005/events',{
+             type: 'CommentUpdated',
+             data:{
+                 id,
+                 postId,
+                 content,
+                 status
+             }
+         })
+     }
 
     res.send('');
 })
--- Blog/moderation/index.js
+++ Blog/moderation/index.js
@@ -4,7 +4,23 @@ const app = express();
 
 app.use(express.json())
 
-app.post('/events',(req,res)=>{})
+app.post('/events',async (req,res)=>{
+    const {type, data} = req.body;
+    if(type==='CommentCreated'){
+        const status = data.content.includes('orange')? 'rejected':'approved';
+
+        await axios.post('http://localhost:4005/events',{
+            type: 'CommentModerated',
+            data:{
+                id: data.id,
+                postId: data.postId,
+                content:data.content,
+                status
+            }
+        })
+    }
+    res.send('');
+})
 app.listen(4003,()=>{
     console.log("Moderation server in running on port 4003");
 })
