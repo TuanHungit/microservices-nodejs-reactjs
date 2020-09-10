f01fd6e34e68a0c2e1cfd0c299d52dc457e25890
NguyenTuanHung14 <56195033+NguyenTuanHung14@users.noreply.github.com>
2020-09-10 22:43:48 +0700

    ingress-nginx setup

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -4,6 +4,10 @@ import { json } from 'body-parser';
 const app = express();
 app.use(json());
 
+app.get('/api/user/currentuser', (req, res) => {
+  res.send('Hi there!');
+});
+
 app.listen(3000, () => {
-  console.log('[auth-service]: Listening on port 3000!');
+  console.log('[auth-service]: listening on port 3000!!!');
 });
