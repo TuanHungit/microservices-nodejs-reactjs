367fd3ffe061f8dbdd6b9d2b3938e4fe72fe353c
NguyenTuanHung14 <56195033+NguyenTuanHung14@users.noreply.github.com>
2020-09-11 14:17:24 +0700

    handling validation errors

---
+++ ticketing/auth/src/routes/signout.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+const router = express.Router();
+router.get('/api/users/signout', (req, res) => {
+  res.send('Hi there!');
+});
+
+export { router as signoutRouter };
