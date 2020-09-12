d46c39fafadb16ec7564a206561de81f042f2546
NguyenTuanHung14 <56195033+NguyenTuanHung14@users.noreply.github.com>
2020-09-12 12:47:29 +0700

    create auth model

---
+++ ticketing/auth/src/models/user.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  eamil: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+export { User };
