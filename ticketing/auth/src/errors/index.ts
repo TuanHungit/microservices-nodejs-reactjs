35727ec2abcbf8d1cfb5695477b1074ffb583ffc
NguyenTuanHung14 <56195033+NguyenTuanHung14@users.noreply.github.com>
2020-09-13 19:32:17 +0700

    add password hashing

---
+++ ticketing/auth/src/errors/index.ts
@@ -0,0 +1,4 @@
+export { BadRequestError } from './bad-request-error';
+export { DatabaseConnectionError } from './database-connection-error';
+export { NotFoundError } from './not-found-error';
+export { RequestValidationError } from './request-validation-error';
